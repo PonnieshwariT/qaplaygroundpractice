e66d83bfdd992b521b065b63e528bd21d902f4c9
Ponnieshwari Thirumurugesh <u502556@wns.com>
2026-06-08 23:18:56 +0530

    Remove onboarding modal test

--- tests/onboardingModal.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-import { OnboardingModalPage } from '../pages/OnboardingModalPage';
-
-test('TC_Onboarding Modal Test', async ({ page }) => {
-  const modalPage = new OnboardingModalPage(page);
-
-  await modalPage.navigate();
-  await modalPage.verifyApplicationLaunch();
-});
